9153ec61e1900d6710c8142bb0f88f6f15be6354
shoumique <shoumique@gmail.com>
2025-07-05 07:45:38 +0600

    Updated suspect creation url

--- FR-api/core/urls.py
+++ FR-api/core/urls.py
@@ -3,5 +3,5 @@ from .views import AIWebhookView, SuspectCreateView
 
 urlpatterns = [
     path('ai-webhook/', AIWebhookView.as_view(), name='ai_webhook'),
-    path('suspects/', SuspectCreateView.as_view(), name='create_suspect'),
+    path('suspect/', SuspectCreateView.as_view(), name='create_suspect'),
 ]
